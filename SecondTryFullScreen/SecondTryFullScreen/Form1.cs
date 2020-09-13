701e8b54fedd4a6be635af7f3c205f21f6fbdde5
Samjacobs234 <71222022+Samjacobs234@users.noreply.github.com>
2020-09-13 10:33:21 -0500

    Initial Upload
    This is an upload of a poor example of using Magnification API to create a window. I still have a fullscreen window, but was created using a window handle and a click through form. May be good for reference

---
+++ SecondTryFullScreen/SecondTryFullScreen/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace SecondTryFullScreen
+{
+
+
+    public partial class Form1 : Form
+    {
+        public Form form2;
+        public Bitmap target;
+        public Rectangle rectBounding;
+        public float[] Negative;
+        public float[] Blueinverse;
+        private Form formformag;
+        public bool updatemag = false;
+
+        private IntPtr hwndMag;
+        private float magnification;
+        private RECT magWindowRect = new RECT();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Negative = new float[] {
+                -1.0f,  0.0f,  0.0f,  0.0f,  0.0f ,
+                0.0f,  -1.0f,  0.0f,  0.0f, 0.0f,
+                0.0f,  0.0f,  -1.0f, 0.0f,  0.0f ,
+                0.0f,  0.0f,  0.0f,  1.0f,  0.0f ,
+                1.0f,  1.0f,  1.0f,  0.0f,  1.0f};
+
+            Blueinverse = new float[] {
+                0.0f,  0.0f,  -0.3f,  0.0f,  0.0f ,
+                0.0f,  0.0f,  -0.6f,  0.0f, 0.0f,
+                0.0f,  0.0f,  -0.1f, 0.0f,  0.0f ,
+                0.0f,  0.0f,  0.0f,  1.0f,  0.0f ,
+                0.0f,  0.0f,  1.0f,  0.0f,  1.0f};
+            NativeMethods.MagInitialize();
+           // NativeMethods.MagSetFullscreenColorEffect(Blueinverse);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form formformag = new Form();
+            updatemag = true;
+            magnification = 1.0f;
+            this.formformag = formformag;
+            SetupMagnifier();
+
+
+        }
+
+ 
+        public void SetupMagnifier()
+        {
+            Blueinverse = new float[] {
+                0.0f,  0.0f,  -0.3f,  0.0f,  0.0f ,
+                0.0f,  0.0f,  -0.6f,  0.0f, 0.0f,
+                0.0f,  0.0f,  -0.1f, 0.0f,  0.0f ,
+                0.0f,  0.0f,  0.0f,  1.0f,  0.0f ,
+                0.0f,  0.0f,  1.0f,  0.0f,  1.0f};
+
+            IntPtr hInst;
+            hInst = NativeMethods.GetModuleHandle(null);
+            formformag.AllowTransparency = true;
+            formformag.TransparencyKey = Color.Empty;
+            formformag.Opacity = 255;
+            formformag.FormBorderStyle = FormBorderStyle.None;
+            formformag.WindowState = FormWindowState.Maximized;
+            int initialStyle = NativeMethods.GetWindowLong(formformag.Handle, -20);
+            NativeMethods.SetWindowLong(formformag.Handle, -20, initialStyle | 0x80000 | 0x20);
+
+
+
+            NativeMethods.GetClientRect(formformag.Handle, ref magWindowRect);
+            hwndMag = NativeMethods.CreateWindow((int)ExtendedWindowStyles.WS_EX_CLIENTEDGE, NativeMethods.WC_MAGNIFIER,
+                "MagnifierWindow", (int)WindowStyles.WS_CHILD | (int)MagnifierStyle.MS_SHOWMAGNIFIEDCURSOR |
+                (int)WindowStyles.WS_VISIBLE,
+                magWindowRect.left, magWindowRect.top, magWindowRect.right, magWindowRect.bottom, formformag.Handle, IntPtr.Zero, hInst, IntPtr.Zero);
+
+
+            Transformation matrix = new Transformation(magnification);
+            NativeMethods.MagSetWindowTransform(hwndMag, ref matrix);
+            NativeMethods.MagSetColorEffect(hwndMag, Negative);
+           formformag.Show();
+
+
+            Rectangle rectBounding = new Rectangle();
+            rectBounding.X = 0;
+            rectBounding.Y = 0;
+            rectBounding.Width = 1920;
+            rectBounding.Height = 1080;
+            Bitmap target = new Bitmap(rectBounding.Width, rectBounding.Height);
+            using (Graphics g = Graphics.FromImage(target))
+            {
+                g.CopyFromScreen(0, 0, 0, 0, new Size(rectBounding.Width, rectBounding.Height));
+            }
+
+
+            target.Save("foo2.png", System.Drawing.Imaging.ImageFormat.Png);
+
+
+            // HBITMAP hBitmap =(HBITMAP)hwndMag
+            //Bitmap b = Bitmap.FromHbitmap(hBitmap);
+            //ForDrawToBitmap(b, new Rectangle(0, 0, 1080, 1920));
+            //b.Save("Test.bmp");
+
+
+
+        }
+
+        public virtual void UpdateMaginifier()
+        {
+           /* if ((!initialized) || (hwndMag == IntPtr.Zero))
+                return;*/
+
+            POINT mousePoint = new POINT();
+            RECT sourceRect = new RECT();
+
+            NativeMethods.GetCursorPos(ref mousePoint);
+
+            int width = (int)((magWindowRect.right - magWindowRect.left) / magnification);
+            int height = (int)((magWindowRect.bottom - magWindowRect.top) / magnification);
+
+            sourceRect.left = mousePoint.x - width / 2;
+            sourceRect.top = mousePoint.y - height / 2;
+
+
+            // Don't scroll outside desktop area.
+            if (sourceRect.left < 0)
+            {
+                sourceRect.left = 0;
+            }
+            if (sourceRect.left > NativeMethods.GetSystemMetrics(NativeMethods.SM_CXSCREEN) - width)
+            {
+                sourceRect.left = NativeMethods.GetSystemMetrics(NativeMethods.SM_CXSCREEN) - width;
+            }
+            sourceRect.right = sourceRect.left + width;
+
+            if (sourceRect.top < 0)
+            {
+                sourceRect.top = 0;
+            }
+            if (sourceRect.top > NativeMethods.GetSystemMetrics(NativeMethods.SM_CYSCREEN) - height)
+            {
+                sourceRect.top = NativeMethods.GetSystemMetrics(NativeMethods.SM_CYSCREEN) - height;
+            }
+            sourceRect.bottom = sourceRect.top + height;
+
+            // Set the source rectangle for the magnifier control.
+            NativeMethods.MagSetWindowSource(hwndMag, sourceRect);
+
+            // Reclaim topmost status, to prevent unmagnified menus from remaining in view. 
+            NativeMethods.SetWindowPos(formformag.Handle, NativeMethods.HWND_TOPMOST, 0, 0, 0, 0,
+                (int)SetWindowPosFlags.SWP_NOACTIVATE | (int)SetWindowPosFlags.SWP_NOMOVE | (int)SetWindowPosFlags.SWP_NOSIZE);
+
+            // Force redraw.
+            NativeMethods.InvalidateRect(hwndMag, IntPtr.Zero, true);
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            while (updatemag == true)
+            {
+
+                UpdateMaginifier();
+
+
+
+                Rectangle rectBounding = new Rectangle();
+                rectBounding.X = 0;
+                rectBounding.Y = 0;
+                rectBounding.Width = 1920;
+                rectBounding.Height = 1080;
+                Bitmap target = new Bitmap(rectBounding.Width, rectBounding.Height);
+                using (Graphics g = Graphics.FromImage(target))
+                {
+                    g.CopyFromScreen(0, 0, 0, 0, new Size(rectBounding.Width, rectBounding.Height));
+                }
+
+
+                target.Save("foo2.png", System.Drawing.Imaging.ImageFormat.Png);
+
+
+            }
+        }
+    }
+
+
+}
---
+++ SecondTryFullScreen/SecondTryFullScreen/NativeMethods.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+namespace SecondTryFullScreen
+{
+    class NativeMethods
+    {
+
+
+
+        public const int SM_CXSCREEN = 0;
+        public const int SM_CYSCREEN = 1;
+        public const string WC_MAGNIFIER = "Magnifier";
+        public static IntPtr HWND_TOPMOST = new IntPtr(-1);
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        #region user32.dll
+        ///        //user32 library will be required to use "getwindowlong" and "setwindowlong" which are ESSENTIAL for a click through window
+        //Note how each method we want to use is importated seperately
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool InvalidateRect(IntPtr hWnd, IntPtr rect, [MarshalAs(UnmanagedType.Bool)] bool erase);
+
+        [DllImport("user32.dll")]
+        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetCursorPos(ref POINT pt);
+
+        [DllImport("user32.dll")]
+        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern int GetSystemMetrics(int nIndex);
+
+
+        [DllImport("user32.dll")]
+        static extern bool SetLayeredWindowAttributes(IntPtr hwnd, uint crKey, byte bAlpha, uint dwFlags);
+        /// The main entry point for the application.
+
+
+        #endregion
+
+        #region Maginification.dll
+
+
+        [DllImport("Magnification.dll", CallingConvention = CallingConvention.StdCall)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool MagSetWindowTransform(IntPtr hwnd, ref Transformation pTransform);
+
+        [DllImport("Magnification.dll", CallingConvention = CallingConvention.StdCall)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool MagSetWindowSource(IntPtr hwnd, RECT rect);
+
+
+        [DllImport("Magnification.dll", CallingConvention = CallingConvention.Winapi, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool MagInitialize();
+
+        [DllImport("Magnification.dll", CallingConvention = CallingConvention.Winapi, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool MagUninitialize();
+
+        [DllImport("Magnification.dll", CallingConvention = CallingConvention.Winapi, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        // ref keyword necessary for X86, not sure why... (crash on call otherwise)
+        public static extern bool MagSetColorEffect(IntPtr hwnd, float[] pEffect);
+
+        [DllImport("Magnification.dll", CallingConvention = CallingConvention.Winapi, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool MagSetFullscreenColorEffect(float[] pEffect);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int cx, int cy, int flags);
+
+        [DllImport("Magnification.dll", CallingConvention = CallingConvention.Winapi, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool MagSetFullscreenTransform(float magLevel, int xOffset, int yOffset);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetClientRect(IntPtr hWnd, [In, Out] ref RECT rect);
+
+        [DllImport("user32.dll", EntryPoint = "CreateWindowExW", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public extern static IntPtr CreateWindow(int dwExStyle, string lpClassName, string lpWindowName, int dwStyle, int x, int y, int nWidth, int nHeight, IntPtr hWndParent, IntPtr hMenu, IntPtr hInstance, IntPtr lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetLayeredWindowAttributes(IntPtr hwnd, int crKey, byte bAlpha, uint dwFlags);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        public static extern IntPtr GetModuleHandle([MarshalAs(UnmanagedType.LPWStr)] string modName);
+
+        [DllImport("user32.dll")]
+       public static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetDesktopWindow();
+
+
+
+
+    }
+}
+#endregion
